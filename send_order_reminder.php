95b6020706975469be3e8b17801cc909724e1904
Scott C Wilson <scottcwilson@gmail.com>
2020-02-27 12:42:42 -0500

    Updates

--- send_order_reminder.php
+++ send_order_reminder.php
@@ -1,6 +1,14 @@
 <?php
 require("includes/application_top.php");
-if ($_GET['key'] != SEND_REMINDER_CRON_KEY) exit(); 
+// don't run checks on localhost
+if (strpos(HTTP_SERVER, "localhost") === false) { 
+   if (getenv('CRON_MODE')) {
+     // Call from Cron
+   } else if ($_GET['key'] != SEND_REMINDER_CRON_KEY) {
+     // Call from web with bad arg - possibly attack
+     die("Invalid web"); // norefreshlog
+   }
+}
 
 $list = $db->Execute("SELECT orders_id, customers_name, customers_email_address, reminder_delay, reminder_email, date_purchased FROM " . TABLE_ORDERS . " WHERE reminder_delay > 0 AND reminder_sent = 0"); 
 
